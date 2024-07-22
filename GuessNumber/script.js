11d2963172ccb1557e162025b540f20426926599
Divyaanshu Agrawal <divyaanshuagrawal2711@gmail.com>
2024-07-22 10:23:05 +0530

    Delete GuessNumber directory

--- GuessNumber/script.js
+++
@@ -1,6 +0,0 @@
-let randomNo=(parseInt(Math.random()*100 + 1));
-
-const submit = document.querySelector("#subt")
-const userInput  = document.querySelector("#guessField").value
-const guessSlot= document.querySelector(".lastResult")
-const loworhigh = document.querySelector('.lowOrHigh')
